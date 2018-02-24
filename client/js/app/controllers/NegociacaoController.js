a782f831e3556c9c0ba624df7e2908ab19ef20c6
crison <kristiano.gon@gmail.com>
2018-02-24 01:38:39 -0300

    Update view

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,28 +6,31 @@ class NegociacaoController {
     this._data = $('#data');
     this._quantidade = $('#quantidade');
     this._valor = $('#valor');
-    this._listaNegociacoes = new ListaNegociacoes();
-    this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-    this._negociacoesView.update(this._listaNegociacoes);
-    this._mensagem = new Mensagem();
     
-    this._mensagemView = new MensagemView($('#mensagemView'));
-    this._mensagemView.update(this._mensagem);
+    this._listaNegociacoes = new Bind(
+      new ListaNegociacoes(), 
+      new NegociacoesView($('#negociacoesView')),
+      ['adiciona', 'esvazia']
+    );
+    
+    this._mensagem = new Bind(
+      new Mensagem(),
+      new MensagemView($('#mensagemView')),
+      ['texto']
+    );
   }
 
   adiciona(event){
 
     event.preventDefault();
-  
     this._listaNegociacoes.adiciona(this._criaNegociacao());
     this._mensagem.texto = 'Sucesso ao adcionar negociação!';
     this._limpaFormulario();
-    
-    this._mensagemView.update(this._mensagem);
-    this._negociacoesView.update(this._listaNegociacoes);
+
   }
 
   _criaNegociacao() {
+
     return new Negociacao(
       DateHelper.textoParaData(this._data.value),
       this._quantidade.value,
@@ -36,11 +39,16 @@ class NegociacaoController {
   }
   
   _limpaFormulario() {
+
     this._data.value = '';
     this._quantidade.value = 1;
     this._valor.value = 0;
-
     this._data.focus();
   }
 
+  apaga() {
+
+    this._listaNegociacoes.esvazia();
+    this._mensagem.texto = 'Negociação apagada com sucesso!';
+  }
 }
